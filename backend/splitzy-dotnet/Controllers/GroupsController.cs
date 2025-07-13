e0a9be22bc69ae6c4b8863ba2da1691e1aa36265
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2025-07-13 18:29:15 +0530

    Sample Add

---
+++ backend/splitzy-dotnet/Controllers/GroupsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using splitzy_dotnet.DTO;
+using splitzy_dotnet.Models;
+
+namespace splitzy_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GroupsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAllGroups")]
+        public IActionResult GetAllGroups()
+        {
+            var groupsFromDB = _context.Groups.ToList();
+
+            var groups = groupsFromDB.Select(g => new GroupDTO
+            {
+                GroupId = g.GroupId,
+                Name = g.Name,
+                CreatedAt = g.CreatedAt,
+            });
+
+            return Ok(groups);
+        }
+    }
+}
---
+++ backend/splitzy-dotnet/DTO/ExpenseSplitDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace splitzy_dotnet.DTO
+{
+    public class ExpenseSplitDTO
+    {
+        public int Id { get; set; }
+        public int ExpenseId { get; set; }
+        public int UserId { get; set; }
+        public decimal OwedAmount { get; set; }
+    }
+}
